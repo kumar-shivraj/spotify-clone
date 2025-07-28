dacd5b9ec0e54d7e2941a7334d3181e0d17b9906
kumar-shivraj <shivrajkumar11math@gmail.com>
2025-07-28 09:56:54 +0530

    spotify-clone: add the required assets

---
+++ src/assets/assets.js
@@ -0,0 +1,184 @@
+import bellIcon from './bell.png'
+import homeIcon from './home.png'
+import likeIcon from './like.png'
+import loopIcon from './loop.png'
+import micIcon from './mic.png'
+import nextIcon from './next.png'
+import playIcon from './play.png'
+import pauseIcon from './pause.png'
+import playsIcon from './plays.png'
+import prevIcon from './prev.png'
+import searchIcon from './search.png'
+import shuffleIcon from './shuffle.png'
+import speakerIcon from './speaker.png'
+import stackIcon from './stack.png'
+import zoomIcon from './zoom.png'
+import plusIcon from './plus.png'
+import arrowIcon from './arrow.png'
+import miniPlayerIcon from './mini-player.png'
+import queueIcon from './queue.png'
+import volumeIcon from './volume.png'
+import arrowRight from './right_arrow.png'
+import arrowLeft from './left_arrow.png'
+import spotifyLogo from './spotify_logo.png'
+import clockIcon from './clock_icon.png'
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+import img3 from './img3.jpg'
+import img4 from './img4.jpg'
+import img5 from './img5.jpg'
+// eslint-disable-next-line
+import img6 from './img6.jpg'
+import img7 from './img7.jpg'
+import img8 from './img8.jpg'
+import img9 from './img9.jpg'
+import img10 from './img10.jpg'
+import img11 from './img11.jpg'
+import img12 from './img12.jpg'
+// eslint-disable-next-line
+import img13 from './img13.jpg'
+import img14 from './img14.jpg'
+import img15 from './img15.jpg'
+import img16 from './img16.jpg'
+import song1 from './song1.mp3'
+import song2 from './song2.mp3'
+import song3 from './song3.mp3'
+
+export const assets = {
+  bellIcon,
+  homeIcon,
+  likeIcon,
+  loopIcon,
+  micIcon,
+  nextIcon,
+  playIcon,
+  playsIcon,
+  prevIcon,
+  searchIcon,
+  shuffleIcon,
+  speakerIcon,
+  stackIcon,
+  zoomIcon,
+  plusIcon,
+  arrowIcon,
+  miniPlayerIcon,
+  volumeIcon,
+  queueIcon,
+  pauseIcon,
+  arrowLeft,
+  arrowRight,
+  spotifyLogo,
+  clockIcon
+}
+
+export const albumsData = [
+  {
+    id: 0,
+    name: 'Top 50 Global',
+    image: img8,
+    desc: 'Your weekly update of the most played tracks',
+    bgColor: '#2a4365'
+  },
+  {
+    id: 1,
+    name: 'Top 50 India',
+    image: img9,
+    desc: 'Your weekly update of the most played tracks',
+    bgColor: '#22543d'
+  },
+  {
+    id: 2,
+    name: 'Trending India',
+    image: img10,
+    desc: 'Your weekly update of the most played tracks',
+    bgColor: '#742a2a'
+  },
+  {
+    id: 3,
+    name: 'Trending Global',
+    image: img16,
+    desc: 'Your weekly update of the most played tracks',
+    bgColor: '#44337a'
+  },
+  {
+    id: 4,
+    name: 'Mega Hits,',
+    image: img11,
+    desc: 'Your weekly update of the most played tracks',
+    bgColor: '#234e52'
+  },
+  {
+    id: 5,
+    name: 'Happy Favorites',
+    image: img15,
+    desc: 'Your weekly update of the most played tracks',
+    bgColor: '#744210'
+  }
+]
+
+export const songsData = [
+  {
+    id: 0,
+    name: 'Song One',
+    image: img1,
+    file: song1,
+    desc: 'Put a smile on your face with these happy tunes',
+    duration: '3:00'
+  },
+  {
+    id: 1,
+    name: 'Song Two',
+    image: img2,
+    file: song2,
+    desc: 'Put a smile on your face with these happy tunes',
+    duration: '2:20'
+  },
+  {
+    id: 2,
+    name: 'Song Three',
+    image: img3,
+    file: song3,
+    desc: 'Put a smile on your face with these happy tunes',
+    duration: '2:32'
+  },
+  {
+    id: 3,
+    name: 'Song Four',
+    image: img4,
+    file: song1,
+    desc: 'Put a smile on your face with these happy tunes',
+    duration: '2:50'
+  },
+  {
+    id: 4,
+    name: 'Song Five',
+    image: img5,
+    file: song2,
+    desc: 'Put a smile on your face with these happy tunes',
+    duration: '3:10'
+  },
+  {
+    id: 5,
+    name: 'Song Six',
+    image: img14,
+    file: song3,
+    desc: 'Put a smile on your face with these happy tunes',
+    duration: '2:45'
+  },
+  {
+    id: 6,
+    name: 'Song Seven',
+    image: img7,
+    file: song1,
+    desc: 'Put a smile on your face with these happy tunes',
+    duration: '2:18'
+  },
+  {
+    id: 7,
+    name: 'Song Eight',
+    image: img12,
+    file: song2,
+    desc: 'Put a smile on your face with these happy tunes',
+    duration: '2:35'
+  }
+]
